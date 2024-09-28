d791c7104c04573ed1f12a9b1cbd670474391766
vishnubaalan <vishnubaalan1211@gmail.com>
2024-09-29 02:56:47 +0530

    Chapter 11 Bug resolved

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -3,11 +3,6 @@ import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchRevenue } from '@/app/lib/data';
 
-// export default async function RevenueChart({
-//   revenue,
-// }: {
-//   revenue: Revenue[];
-// }) {
 
 export default async function RevenueChart() { 
     const revenue = await fetchRevenue();
@@ -29,7 +24,7 @@ export default async function RevenueChart() {
         <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4">
           <div
             className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
-            style={{ height: `${chartHeight}px` }}
+            style={{ height: chartHeight + 'px' }}
           >
             {yAxisLabels.map((label) => (
               <p key={label}>{label}</p>
@@ -40,9 +35,8 @@ export default async function RevenueChart() {
             <div key={month.month} className="flex flex-col items-center gap-2">
               <div
                 className="w-full rounded-md bg-blue-300"
-                style={{
-                  height: `${(chartHeight / topLabel) * month.revenue}px`,
-                }}
+                style={{ height: (chartHeight / topLabel) * month.revenue + 'px' }}
+
               />
               <p className="-rotate-90 text-sm text-gray-400 sm:rotate-0">
                 {month.month}
